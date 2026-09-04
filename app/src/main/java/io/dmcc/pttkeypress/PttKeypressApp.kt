61bd059c8ac72a30d00eec280a8b76bb4d6c996e
Danny McClelland <dmcc365@gmail.com>
2026-09-04 22:12:29 +0100

    Wire direct VoxDMR output

--- app/src/main/java/io/dmcc/pttkeypress/PttKeypressApp.kt
+++ app/src/main/java/io/dmcc/pttkeypress/PttKeypressApp.kt
@@ -3,10 +3,10 @@ package io.dmcc.pttkeypress
 import android.app.Application
 import io.dmcc.pttkeypress.ble.PttBleManager
 import io.dmcc.pttkeypress.data.PttRepository
-import io.dmcc.pttkeypress.inject.ShizukuKeyInjector
+import io.dmcc.pttkeypress.output.VoxDmrBridge
 
 class PttKeypressApp : Application() {
     val repository by lazy { PttRepository(this) }
-    val injector by lazy { ShizukuKeyInjector(this) }
-    val bleManager by lazy { PttBleManager(this, repository, injector) }
+    val voxDmrBridge by lazy { VoxDmrBridge(this) }
+    val bleManager by lazy { PttBleManager(this, repository, voxDmrBridge) }
 }
